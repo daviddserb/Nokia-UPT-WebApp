5e62ee5c047d326a980fab0718a28296cda196e9
serbdavid7 <daviddserb@yahoo.com>
2022-03-26 09:53:14 +0200

    firstTaskCompleteUpdated

--- webapp/urls.py
+++ webapp/urls.py
@@ -2,14 +2,11 @@ from django.urls import path
 
 from . import views
 
-# nu stiu daca am nevoie de asta
-# app_name = 'webapp' # webapp -> templates -> webapp
-
 urlpatterns = [
-    # pot sa pui ce path vrei la ''
-    path('', views.index, name='index'),
-    path('<int:notepad_id>/', views.testRunDetails, name='testRunDetails'),
-    
+    # pot sa pui ce path vrei
+    path('logs', views.index, name='index'),
+    path('logs/<int:notepad_id>', views.testRunDetails, name='testRunDetails'),
+
 #pt. testing (de inteles)
     path('<int:question_id>/results/', views.results, name='results'),  # ex: /polls/5/results/
     path('<int:question_id>/vote/', views.vote, name='vote'), # ex: /polls/5/vote/
